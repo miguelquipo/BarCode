54fa4d9294cdd0c160f7eb957f0475a9cb52e7f7
USUARIO <quipomiguel42@gmail.com>
2023-12-02 09:12:42 -0500

    Agregado registro con SQL server

--- PHP/conexion.php
+++ PHP/conexion.php
@@ -12,6 +12,8 @@ class conexion{
 
 $con = new conexion();
 $db = $con->getConexion();
+$guid = trim(com_create_guid(), '{}');
+
 
 if($db != null){
     echo "Conexión exitosa";
@@ -19,39 +21,27 @@ if($db != null){
     
 
     // Preparar la consulta SQL
-    $stmt = $db->prepare("INSERT INTO R_Trabajador (Id,TenantId,IsDeleted,NombreTrab,ApellidoTrab,BloqueId,TipoTrabajadorId,ExtraProperties,ConcurrencyStamp,CreationTime,CreatorId,LastModificationTime,LastModifierld,TagTrab,Imagen,ProduccionType,cedula,correo) VALUES (:null, :null, :nombre, :apellido, :null,:TipoTrabajadorId,:null,:null,:null,:null,:null,null,:token,:null,:null,:cedula,:correo)");
+    $stmt = $db->prepare("INSERT INTO R_Trabajador (id,TenantId,IsDeleted,NombreTrab, ApellidoTrab,BloqueId,TipoTrabajadorId,ExtraProperties,ConcurrencyStamp,CreationTime,CreatorId,LastModificationTime,LastModifierId,TagTrab,Imagen,ProduccionType,cedula,correo) VALUES (:guide, NULL, 0, :nombre, :apellido, 'DDB17246-9773-1C56-EEE8-3A04A48E831F', :trabajador, NULL,NULL,'2022-07-14 12:35:26.5252690','D51F24DA-2450-5244-CB65-3A045A90DB0A',NULL,NULL,444400000018, NULL,NULL,:cedula, :correo)");
 
-    // Vincular parámetros
+    $stmt->bindParam(':guide', $guid);
     $stmt->bindParam(':cedula', $_POST['cedula']);
-    $stmt->bindParam(':token', $_POST['token']);
     $stmt->bindParam(':apellido', $_POST['apellido']);
-    $stmt->bindParam(':nombre', $_POST['NombreTrab']);
+    $stmt->bindParam(':nombre', $_POST['nombre']);
     $stmt->bindParam(':correo', $_POST['correo']);
     $stmt->bindParam(':trabajador', $_POST['trabajador']);
 
-    // insertar una fila
-    $stmt->execute();
+$stmt->execute();
 
-    echo '<script>
-    swal({
-        title: "¡Éxito!",
-        text: "Nuevo registro creado exitosamente",
-        icon: "success",
-        button: "OK",
-    });
-  </script>';
-} catch (PDOException $e) {
-// Print a JavaScript code to show a SweetAlert
-echo '<script>
-    swal({
-        title: "¡Error!",
-        text: "No se pudo crear el registro",
-        icon: "error",
-        button: "OK",
-    });
-  </script>';
-}
+
+    echo "datos cargados";
+      
+    } catch (PDOException $e) {
+        echo "Error al ejecutar la consulta: " . $e->getMessage();
+    }
+    
+    
+
 }else{
-echo "Error al conectar a la base de datos";
+    echo "Error al conectar a la base de datos";
 }
 ?>
